0aa8aeb2d04813a619bc72c7abc42fb79fdad66f
vineet <vineet.22566@sscbs.du.ac.in>
2025-04-01 10:50:28 +0530

    login page design

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,13 @@
 import { SignIn } from '@clerk/nextjs'
+import { dark } from "@clerk/themes";
 
 export default function Page() {
 
   return (
-    <main className=' h-screen w-screen flex justify-center items-center'>
-        <SignIn />
+    <main className=' h-screen w-screen flex justify-center items-center light'>
+        <SignIn appearance={{
+            baseTheme: dark,
+        }}  />
     </main>)
     
 }
